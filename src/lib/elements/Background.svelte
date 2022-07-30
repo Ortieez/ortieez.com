67d3fd7b76abf917fb19b5b792665c4a58f089c8
Adam Lipert <a.lipert@protonmail.com>
2022-07-30 20:58:05 +0200

    Add background component; Add home page

---
+++ src/lib/elements/Background.svelte
@@ -0,0 +1,72 @@
+<style>
+    .line {
+        display: block;
+        background: transparent;
+        background-color: #EBDBB2;
+        position: absolute
+    }
+
+    .left-vertical {
+        min-height: 100vh;
+        width: 2px;
+        left: 6vw;
+    }
+
+    .right-vertical {
+        min-height: 100vh;
+        width: 2px;
+        right: 6vw;
+    }
+
+    .top-horizontal {
+        min-height: 2px;
+        width: 100%;
+        left: 128;
+        top: 8vh;
+    }
+
+    .bottom-horizontal {
+        min-height: 2px;
+        width: 100%;
+        left: 128;
+        bottom: 8vh;
+    }
+
+    .bg {
+        position: fixed;
+        top: -50%;
+        left: -50%;
+        right: -50%;
+        bottom: -50%;
+        width: 200%;
+        height: 200vh;
+        background: transparent url('../../assets/Rectangle.png') repeat 0 0;
+        filter: contrast(2);
+        background-repeat: repeat;
+        animation: bg-animation .2s infinite;
+        opacity: .8;
+        visibility: visible;
+    }
+
+    @keyframes bg-animation {
+        0% { transform: translate(0,0) }
+        10% { transform: translate(-5%,-5%) }
+        20% { transform: translate(-10%,5%) }
+        30% { transform: translate(5%,-10%) }
+        40% { transform: translate(-5%,15%) }
+        50% { transform: translate(-10%,5%) }
+        60% { transform: translate(15%,0) }
+        70% { transform: translate(0,10%) }
+        80% { transform: translate(-15%,0) }
+        90% { transform: translate(10%,5%) }
+        100% { transform: translate(5%,0) }
+    }
+</style>
+
+<main>
+    <span class="left-vertical line"></span>
+    <span class="right-vertical line"></span>
+    <span class="top-horizontal line"></span>
+    <span class="bottom-horizontal line"></span>
+    <div class="bg"></div>
+</main>
---
+++ src/lib/routes/Home.svelte
@@ -0,0 +1,49 @@
+<script>
+    import Background from '../elements/Background.svelte';
+</script>
+
+<main>
+    <div class="center">
+        <div class="logo">
+            <img src="src/assets/logo/Logo-Light.png" alt="logo">
+        </div>
+        <div class="text">
+            <h1>student</h1>
+            <h1 class="push">developer</h1>
+        </div>
+    </div>
+    <Background />
+</main>
+
+<style>
+    .logo,
+    .text {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        
+    }
+
+    .text {
+        display: flex;
+        flex-direction: column;
+        mix-blend-mode: difference;
+        font-size: 3vw;
+    }
+
+    .text h1 {
+        color: #EBDBB2;
+        margin: 0;
+    }
+
+    .logo img {
+        width: 58.8805166846071vh;
+        opacity: .75;
+        
+    }
+
+    .push {
+        padding-left: 5vw;
+    }
+</style>
