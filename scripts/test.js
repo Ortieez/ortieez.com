fda80984dbc67c7ef39760d65a799e04992e1c7f
Adam Lipert <a.lipert@protonmail.com>
2022-10-30 16:38:37 +0100

    asdasd

--- scripts/test.js
+++ scripts/test.js
@@ -2,36 +2,13 @@ let menu = document.getElementById("test2");
 let paths = document.getElementsByClassName("shard");
 
 const test = () => {
-    
-    for (let i = 0; i< 6; i++) {
-        try {
-            paths[i].style.opacity = "0"
+  let status = menu.style.opacity;
+    console.log(status);
+  for (let i = 0; i < 6; i++) {
+    paths[i].style.opacity = status ? 0.5 : 0;
+  }
 
-        } catch(e) {
-            console.log(e);
-            continue
-        }
-        
-    }
-
-    menu.style.width = "100vw"
-    menu.style.height= "100vh"
-    menu.style.opacity= "1"
-}
-
-const close = () => {
-    for (let i = 0; i< 6; i++) {
-        try {
-            paths[i].style.opacity = ".5"
-        } catch(e) {
-            console.log(e);
-            continue
-        }
-        
-    }
-
-    menu.style.width = "0vw"
-    menu.style.height= "0vh"
-    menu.style.opacity= "0"
-
-}
+  menu.style.width = status ? 0 : "100vw";
+  menu.style.height = status ? 0 : "100vh";
+  menu.style.opacity = status ? 0 : 1;
+};
